cc23416ac12587f378b25d058168a6bd2d60ed12
Jenna Lee <jennajlee92@gmail.com>
2025-03-31 11:37:53 -0400

    Variable Exercises

---
+++ balance.py
@@ -0,0 +1,16 @@
+amount = 1000
+years = 5
+balance = 1000 * 1.05**years
+
+print(balance)
+
+#Augmented Assignment
+
+balance = 1000
+balance *= 1.05
+balance *= 1.05
+balance *= 1.05
+balance *= 1.05
+balance *= 1.05
+
+print(balance)
---
+++ greeter.py
@@ -0,0 +1,8 @@
+name = 'Victor'
+
+def greet(someone):
+    print('Good Morning, ' + someone + '.')
+    print('Good Afternoon, ' + someone + '.')
+    print('Good Evening, ' + someone + '.')
+
+greet(name)

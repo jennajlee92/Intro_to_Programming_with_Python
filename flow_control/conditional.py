351b046b7a99b3457dbe578ab5982a634dc2baf6
Jenna Lee <jennajlee92@gmail.com>
2025-04-07 12:17:42 -0400

    Began flow control chapter

---
+++ flow_control/conditional.py
@@ -0,0 +1,16 @@
+value = int(input('Enter a number: '))
+
+if value == 3:
+    print('value is 3')
+    print('value is an odd number')
+    print('value is a prime number')
+elif value == 4:
+    print('value is 4')
+    print('value is an even number')
+    print('value is NOT a prime number')
+elif value == 9:
+    print('value is 9')
+    print('value is an odd number')
+    print('value is NOT a prime number')
+else:
+    print('value is something else')
